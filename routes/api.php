be144d033cb762937515602890a02c28028300b5
Raya Taufik <rayataufik4@gmail.com>
2024-10-19 14:32:21 +0700

    Adding grup middleware in API

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,19 @@ use App\Http\Controllers\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-// For role doctor
-Route::middleware('auth:sanctum', 'role:doctor')->group(function () {
-    Route::get('profile', [AuthController::class, 'profile']);
-    Route::post('logout', [AuthController::class, 'logout']);
-});
 
-// For role patient
-Route::middleware('auth:sanctum', 'role:patient')->group(function () {
-    Route::get('profile', [AuthController::class, 'profile']);
-    Route::post('logout', [AuthController::class, 'logout']);
-});
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::middleware('role:psychologists')->group(function () {
+        Route::get('profile', [AuthController::class, 'profile']);
+    });
 
-// For role admin
-Route::middleware('auth:sanctum', 'role:admin')->group(function () {
-    Route::get('profile', [AuthController::class, 'profile']);
+
+    Route::middleware('role:patient')->group(function () {
+        Route::get('profile', [AuthController::class, 'profile']);
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        Route::get('profile', [AuthController::class, 'profile']);
+    });
     Route::post('logout', [AuthController::class, 'logout']);
 });
