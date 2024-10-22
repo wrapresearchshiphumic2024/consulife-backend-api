20cddad4d3a67c6317287bb17a6bac4f5329af46
Raya Taufik <rayataufik4@gmail.com>
2024-10-22 19:51:51 +0700

    Create Admin Approval and Detail Psychologist

--- app/Models/Psychologist.php
+++ app/Models/Psychologist.php
@@ -13,7 +13,7 @@ class Psychologist extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class);
     }
 
     public function appointments()
