d0fec1f9abccc0971a1fe8471883002abc3e096a
Raya Taufik <rayataufik4@gmail.com>
2024-11-13 22:18:16 +0700

    Clean code

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -236,37 +236,4 @@ class AdminController extends Controller
             'data' => $psychologist,
         ], 200);
     }
-
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
